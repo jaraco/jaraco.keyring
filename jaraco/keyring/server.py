ee3a2f98102d85a756732fa92bdbb97ac0b6d003
Jason R. Coombs <jaraco@jaraco.com>
2015-01-26 15:43:20 -0500

    Add keyring server

---
+++ jaraco/keyring/server.py
@@ -0,0 +1,35 @@
+import os
+
+import keyring
+import cherrypy
+
+class Keyring:
+	@cherrypy.expose
+	def default(self, service, username):
+		return getattr(self, cherrypy.request.method)(service, username)
+
+	def GET(self, service, username):
+		return keyring.get_password(service, username)
+
+	def PUT(self, service, username):
+		password = cherrypy.request.body
+		keyring.set_password(service, username, password)
+		return 'OK'
+
+	def DELETE(self, service, username):
+		keyring.delete_password(service, username)
+		return 'OK'
+
+	@classmethod
+	def run(cls):
+		config = {
+			'global': {
+				'server.socket_host': '::1',
+				'server.socket_port': int(os.environ.get('PORT', 4273)),
+			}
+		}
+		cherrypy.quickstart(cls(), config=config)
+
+
+if __name__ == '__main__':
+	Keyring.run()
