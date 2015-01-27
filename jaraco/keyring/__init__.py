c249463d8fcbc6ca719fa7f566a8019636c495e9
Jason R. Coombs <jaraco@jaraco.com>
2015-01-26 21:00:29 -0500

    Fleshed out stubbed API calls.

--- jaraco/keyring/__init__.py
+++ jaraco/keyring/__init__.py
@@ -1,15 +1,21 @@
+import requests
 import keyring.backend
 
 class RemoteAgent(keyring.backend.Backend):
+	_url_tmpl = 'http://localhost:4273/{service}/{username}'
+
 	@classmethod
 	def priority(cls):
 		return 3.1
 
 	def get_password(self, service, username):
-		return 'stubbed'
+		url = self._url_tmpl.format(**locals())
+		return requests.get(url).text
 
 	def set_password(self, service, username, password):
-		"stubbed"
+		url = self._url_tmpl.format(**locals())
+		requests.post(url, data=password)
 
 	def delete_password(self, service, username):
-		"stubbed"
+		url = self._url_tmpl.format(**locals())
+		requests.delete(url).raise_for_status()
